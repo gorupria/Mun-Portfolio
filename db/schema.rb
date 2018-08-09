c026e30510de55b2b5a9137814da08483b149dde
Purushot <battle_g5@yahoo.com>
2018-08-09 20:20:30 +0300

    Params column added to impressionist

--- db/schema.rb
+++ db/schema.rb
@@ -13,23 +13,25 @@
 ActiveRecord::Schema.define(version: 2014_06_16_044550) do
 
   create_table "impressions", force: :cascade do |t|
-    t.string "impressionable_type", limit: 255
+    t.string "impressionable_type"
     t.integer "impressionable_id"
     t.integer "user_id"
-    t.string "controller_name", limit: 255
-    t.string "action_name", limit: 255
-    t.string "view_name", limit: 255
-    t.string "request_hash", limit: 255
-    t.string "ip_address", limit: 255
-    t.string "session_hash", limit: 255
+    t.string "controller_name"
+    t.string "action_name"
+    t.string "view_name"
+    t.string "request_hash"
+    t.string "ip_address"
+    t.string "session_hash"
     t.text "message"
     t.text "referrer"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.text "params"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["controller_name", "action_name", "ip_address"], name: "controlleraction_ip_index"
     t.index ["controller_name", "action_name", "request_hash"], name: "controlleraction_request_index"
     t.index ["controller_name", "action_name", "session_hash"], name: "controlleraction_session_index"
     t.index ["impressionable_type", "impressionable_id", "ip_address"], name: "poly_ip_index"
+    t.index ["impressionable_type", "impressionable_id", "params"], name: "poly_params_request_index"
     t.index ["impressionable_type", "impressionable_id", "request_hash"], name: "poly_request_index"
     t.index ["impressionable_type", "impressionable_id", "session_hash"], name: "poly_session_index"
     t.index ["impressionable_type", "message", "impressionable_id"], name: "impressionable_type_message_index"
